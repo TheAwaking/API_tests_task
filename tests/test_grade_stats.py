7cedefde5bb7b28883bf2ea591f571c94b636253
111 <skybless2015@gmail.com>
2025-03-14 17:16:03 +0300

    fix

--- tests/test_grade_stats.py
+++ tests/test_grade_stats.py
@@ -1,34 +1,43 @@
 import pytest
 import requests
 
-from logger.logger import Logger
-from services.university.helpers.grade_helper import GradeHelper
-from utils.api_utils import ApiUtils
-from services.university.university_service import UniversityService
+ENDPOINT = "http://127.0.0.1:8001"
 
 
-class TestStatisticsAPI:
-    ENDPOINT = "http://127.0.0.1:8001"
+def test_get_grades_stats(access_token):
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    response = requests.get(ENDPOINT + "/grades/stats", headers=headers)
+    assert response.status_code == 200
 
-    def test_can_create_task():
-        payload = {
-            "count": 3,
-            "min": 2,
-            "max": 5,
-            "avg": 3.75
-        }
-        response = requests.get(ENDPOINT + "/grades/stats", json=payload)
-        assert response.status_code == 200
 
-        data = response.json()
-        student_id = data["student_id"]
-        teacher_id = data["teacher_id"]
-        group_id = data["group_id"]
-        get_response = requests.get(ENDPOINT + "/grades/stats")
+def test_get_grades_stats_student(access_token): # почему в этом тесте ошибка 422, валидация?
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    response = requests.get(ENDPOINT + "/grades/stats?student_id", headers=headers) # с квери параметром
+    assert response.status_code == 200
 
-        assert get_response.status_code == 200
-        get_grades_data = get_response.json()
-        assert get_grades_data["count"] == payload["count"]
-        assert get_grades_data["min"] == payload["min"]
-        assert get_grades_data["max"] == payload["max"]
-        assert get_grades_data["avg"] == payload["avg"]
+
+def test_create_grade(access_token): # и в этом ошибка с валидацией
+    payload = {
+        "teacher_id": 1,
+        "student_id": 1,
+        "grade": 3
+    }
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+
+    }
+
+    response = requests.post(ENDPOINT + "/grades/", headers=headers, json=payload)
+    assert response.status_code == 201
+
+
+def test_get_grade(access_token):
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    response = requests.get(ENDPOINT + "/grades", headers=headers)
+    assert response.status_code == 200
