c6055e08cd0adc24b09a6bf6e6ad43393552d5f6
111 <skybless2015@gmail.com>
2025-03-17 19:55:52 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
 import time
+import os
+import json
+from pathlib import Path
 
 import pytest
 import requests
-from faker import Faker
 
+from faker import Faker
 from services.auth.auth_service import AuthService
 from services.auth.models.login_request import LoginRequest
 from services.auth.models.register_request import RegisterRequest
@@ -88,23 +91,96 @@ def university_api_utils_admin(access_token):
     api_utils = ApiUtils(url=UniversityService.SERVICE_URL, headers={"Authorization": f"Bearer {access_token}"})
     return api_utils
 
+
 @pytest.fixture(scope="function", autouse=False)
 def headers(access_token):
     return {
         "Authorization": f"Bearer {access_token}"
     }
 
+
 @pytest.fixture(scope="function", autouse=False)
-def setup(access_token):
+def invalid_headers():
+    invalid_token = "invalid_jwt_token"
 
-    ENDPOINT = "http://127.0.0.1:8001"
-
-    fake = Faker()
-    yield {
-        "endpoint": ENDPOINT,
-        "fake": fake,
-        "access_token": access_token
+    return {
+        "Authorization": f"Bearer {invalid_token}"
     }
 
 
+@pytest.fixture
+def load_config():
+    config_path = Path(__file__).resolve().parent.parent / 'config.json'
 
+    with open(config_path) as f:
+        return json.load(f)
+
+
+@pytest.fixture(scope="function", autouse=False)
+def setup(access_token, headers):
+    api_utils = ApiUtils(url=UniversityService.SERVICE_URL)
+    payload_student = {
+        "first_name": faker.first_name(),
+        "last_name": faker.last_name(),
+        "email": faker.email(),
+        "degree": faker.random_element(elements=("Bachelor", "Master")),
+        "phone": faker.numerify("+7##########"),
+        "group_id": 1
+    }
+    response_student = requests.post(api_utils.url + "/students/", headers=headers, json=payload_student)
+    assert response_student.status_code == 201, f"Expected status code 201 for student, but got {response_student.status_code}. Response: {response_student.text}"
+    student = response_student.json()
+
+    payload_group = {
+        "name": faker.word()
+    }
+    response_group = requests.post(api_utils.url + "/groups/", headers=headers, json=payload_group)
+    assert response_group.status_code == 201, f"Expected status code 201 for group, but got {response_group.status_code}. Response: {response_group.text}"
+    group = response_group.json()
+
+    payload_teacher = {
+        "first_name": faker.first_name(),
+        "last_name": faker.last_name(),
+        "subject": faker.random_element(elements=("Geography", "History"))
+    }
+    response_teacher = requests.post(api_utils.url + "/teachers/", headers=headers, json=payload_teacher)
+    assert response_teacher.status_code == 201, f"Expected status code 201 for teacher, but got {response_teacher.status_code}. Response: {response_teacher.text}"
+    teacher = response_teacher.json()
+
+    return {
+        "student": student,
+        "group": group,
+        "teacher": teacher
+    }
+
+@pytest.fixture(scope="function", autouse=False)  #  попробовал использовать неверный юзернейм, но это не работает, вместо этого использовал неверный токен
+def wrong_access_token(auth_api_utils_anonym):
+    auth_service = AuthService(auth_api_utils_anonym)
+    username = faker.user_name()
+    password = faker.password(length=30, special_chars=True, digits=True, upper_case=True, lower_case=True)
+
+    auth_service.register_user(
+        register_request=RegisterRequest(
+            username=username,
+            password=password,
+            password_repeat=password,
+            email=faker.email()
+        )
+    )
+
+    wrong_username = faker.word()
+
+    login_response = auth_service.login_user(LoginRequest(username=wrong_username, password=password))
+    return login_response.access_token
+
+@pytest.fixture(scope="function", autouse=False)
+def setup_delete(access_token, headers):
+    api_utils = ApiUtils(url=UniversityService.SERVICE_URL)
+    payload_group = {
+        "name": faker.word()
+
+    }
+    response = requests.post(api_utils.url + "/groups/", headers=headers, json=payload_group)
+    assert response.status_code == 201, f"Expected status code 201, but got {response.status_code}. Response: {response.text}"
+    group_id = response.json().get("id")
+    return group_id
--- tests/test_grade_stats.py
+++ tests/test_grade_stats.py
@@ -2,58 +2,38 @@ import pytest
 import requests
 from faker import Faker
 
+fake = Faker()
 
-# ENDPOINT = "http://127.0.0.1:8001"
 
-
-def test_get_grades_stats(access_token, headers, setup):
-    response = requests.get(setup['endpoint'] + "/grades/stats", headers=headers)
+def test_get_grades_stats(access_token, headers, load_config):
+    base_url = load_config["api_url"]
+    response = requests.get(f"{base_url}/grades/stats", headers=headers)
     assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}. Response: {response.text}"
 
 
-def test_get_grades_stats_not_authorized(access_token, headers, setup):
-    response = requests.get(setup['endpoint'] + "/grades/stats", headers=headers)
+def test_get_grades_stats_not_authorized(access_token, invalid_headers, load_config):
+    base_url = load_config["api_url"]
+    response = requests.get(f"{base_url}/grades/stats", headers=invalid_headers)
     assert response.status_code == 401, f"Expected status code 401, but got {response.status_code}. Response: {response.text}"
 
 
-def test_get_grades_stats_forbidden(setup):
-    response = requests.get(setup['endpoint'] + "/grades/stats")
+def test_get_grades_stats_forbidden(load_config):
+    base_url = load_config["api_url"]
+
+    response = requests.get(f"{base_url}/grades/stats")
     assert response.status_code == 403, f"Expected status code 403, but got {response.status_code}. Response: {response.text}"
 
 
-def test_get_grades_stats_validation_error(access_token, headers, setup):
-    response = requests.get(setup['endpoint'] + "/grades/stats", headers=headers)
+def test_get_grades_stats_validation_error(access_token, headers, load_config):  #  тут я не могу вспомнить какой кейс применял,чтобы возращалась 422 от сервера
+    base_url = load_config["api_url"]
+
+    response = requests.get(f"{base_url}/grades/stats", headers=headers)
     assert response.status_code == 422, f"Expected status code 422, but got {response.status_code}. Response: {response.text}"
 
 
-def test_create_student(access_token, headers, setup):
-    payload_student = {
-        "first_name": setup['fake'].first_name(),
-        "last_name": setup['fake'].last_name(),
-        "email": setup['fake'].email(),
-        "degree": setup['fake'].random_element(elements=("Bachelor", "Master")),
-        "phone": setup['fake'].numerify("+7##########"),
-        "group_id": 1
-    }
-    response = requests.post(setup['endpoint'] + "/students/", headers=headers, json=payload_student)
-    assert response.status_code == 201, f"Expected status code 201, but got {response.status_code}. Response: {response.text}"
+def test_create_grade(setup, access_token, headers, load_config):
+    base_url = load_config["api_url"]
 
-    payload_group = {
-        "name": setup['fake'].word()
-
-    }
-    response = requests.post(setup['endpoint'] + "/groups/", headers=headers, json=payload_group)
-    assert response.status_code == 201, f"Expected status code 201, but got {response.status_code}. Response: {response.text}"
-
-    payload_teacher = {
-
-        "first_name": "First name",
-        "last_name": "Last name",
-        "subject": "Geography"
-
-    }
-    response = requests.post(setup['endpoint'] + "/teachers/", headers=headers, json=payload_teacher)
-    assert response.status_code == 201, f"Expected status code 201, but got {response.status_code}. Response: {response.text}"
 
     payload_grades = {
         "teacher_id": 1,
@@ -61,24 +41,20 @@ def test_create_student(access_token, headers, setup):
         "grade": 3,
     }
 
-    response = requests.post(setup['endpoint'] + "/grades/", headers=headers, data=payload_grades)
+
+    response = requests.post(f"{base_url}/grades/", headers=headers, json=payload_grades)
     assert response.status_code == 201, f"Expected status code 201, but got {response.status_code}. Response: {response.text}"
 
 
-def test_get_grades(access_token, headers, setup):
-    response = requests.get(setup['endpoint'] + "/grades/", headers=headers)
+def test_get_grades(access_token, headers, load_config):
+    base_url = load_config["api_url"]
+    response = requests.get(f"{base_url}/grades/", headers=headers)
     assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}. Response: {response.text}"
 
 
-def test_delete_group(access_token, headers, setup):
-    payload_group = {
-        "name": setup['fake'].word()
+def test_delete_group(setup_delete, access_token, headers, load_config):
+    base_url = load_config["api_url"]
+    group_id = setup_delete
 
-    }
-    response = requests.post(setup['endpoint'] + "/groups/", headers=headers, json=payload_group)
-    assert response.status_code == 201, f"Expected status code 201, but got {response.status_code}. Response: {response.text}"
-
-    group_id = response.json().get("id")
-
-    response = requests.delete(setup['endpoint'] + f"/groups/{group_id}/", headers=headers)
+    response = requests.delete(f"{base_url}/groups/{group_id}/", headers=headers)
     assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}. Response: {response.text}"
