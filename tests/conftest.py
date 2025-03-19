fe32a7b798063df6235d0f24b1a660a2a5ea76cc
111 <skybless2015@gmail.com>
2025-03-19 20:26:20 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
 import time
-import os
+import jwt
+from datetime import datetime, timedelta
 import json
 from pathlib import Path
+
+
 
 import pytest
 import requests
@@ -15,6 +18,7 @@ from utils.api_utils import ApiUtils
 
 faker = Faker()
 
+SECRET_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImlhdCI6MTY3Mjc2NjAyOCwiZXhwIjoxNjc0NDk0MDI4fQ.kCak9sLJr74frSRVQp0_27BY4iBCgQSmoT3vQVWKzJg"
 
 @pytest.fixture(scope="session", autouse=True)
 def auth_service_readiness():
@@ -100,12 +104,15 @@ def headers(access_token):
 
 
 @pytest.fixture(scope="function", autouse=False)
-def invalid_headers():
-    invalid_token = "invalid_jwt_token"
-
-    return {
-        "Authorization": f"Bearer {invalid_token}"
+def fake_jwt():
+    payload = {
+        "sub": "user_id",
+        "exp": datetime.utcnow() + timedelta(minutes=30),
+        "iat": datetime.utcnow(),
+        "roles": ["user"]
     }
+    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    return token
 
 
 @pytest.fixture
@@ -117,7 +124,7 @@ def load_config():
 
 
 @pytest.fixture(scope="function", autouse=False)
-def setup(access_token, headers):
+def student(headers):
     api_utils = ApiUtils(url=UniversityService.SERVICE_URL)
     payload_student = {
         "first_name": faker.first_name(),
@@ -128,32 +135,39 @@ def setup(access_token, headers):
         "group_id": 1
     }
     response_student = requests.post(api_utils.url + "/students/", headers=headers, json=payload_student)
-    assert response_student.status_code == 201, f"Expected status code 201 for student, but got {response_student.status_code}. Response: {response_student.text}"
-    student = response_student.json()
+    return response_student.json()
 
+@pytest.fixture(scope="function", autouse=False)
+def group(headers):
+    api_utils = ApiUtils(url=UniversityService.SERVICE_URL)
     payload_group = {
         "name": faker.word()
     }
     response_group = requests.post(api_utils.url + "/groups/", headers=headers, json=payload_group)
-    assert response_group.status_code == 201, f"Expected status code 201 for group, but got {response_group.status_code}. Response: {response_group.text}"
-    group = response_group.json()
+    return response_group.json()
 
+@pytest.fixture(scope="function", autouse=False)
+def teacher(headers):
+    api_utils = ApiUtils(url=UniversityService.SERVICE_URL)
     payload_teacher = {
         "first_name": faker.first_name(),
         "last_name": faker.last_name(),
         "subject": faker.random_element(elements=("Geography", "History"))
     }
     response_teacher = requests.post(api_utils.url + "/teachers/", headers=headers, json=payload_teacher)
-    assert response_teacher.status_code == 201, f"Expected status code 201 for teacher, but got {response_teacher.status_code}. Response: {response_teacher.text}"
-    teacher = response_teacher.json()
+    return response_teacher.json()
 
+@pytest.fixture(scope="function", autouse=False)
+def setup_class(student, group, teacher):
     return {
         "student": student,
         "group": group,
         "teacher": teacher
     }
 
-@pytest.fixture(scope="function", autouse=False)  #  попробовал использовать неверный юзернейм, но это не работает, вместо этого использовал неверный токен
+
+@pytest.fixture(scope="function",
+                autouse=False)  # попробовал использовать неверный юзернейм, но это не работает, вместо этого использовал неверный токен
 def wrong_access_token(auth_api_utils_anonym):
     auth_service = AuthService(auth_api_utils_anonym)
     username = faker.user_name()
@@ -173,6 +187,7 @@ def wrong_access_token(auth_api_utils_anonym):
     login_response = auth_service.login_user(LoginRequest(username=wrong_username, password=password))
     return login_response.access_token
 
+
 @pytest.fixture(scope="function", autouse=False)
 def setup_delete(access_token, headers):
     api_utils = ApiUtils(url=UniversityService.SERVICE_URL)
@@ -181,6 +196,5 @@ def setup_delete(access_token, headers):
 
     }
     response = requests.post(api_utils.url + "/groups/", headers=headers, json=payload_group)
-    assert response.status_code == 201, f"Expected status code 201, but got {response.status_code}. Response: {response.text}"
     group_id = response.json().get("id")
     return group_id
--- tests/test_grade_stats.py
+++ tests/test_grade_stats.py
@@ -11,36 +11,41 @@ def test_get_grades_stats(access_token, headers, load_config):
     assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}. Response: {response.text}"
 
 
-def test_get_grades_stats_not_authorized(access_token, invalid_headers, load_config):
+def test_get_grades_stats_not_authorized(access_token, fake_jwt, load_config):
     base_url = load_config["api_url"]
-    response = requests.get(f"{base_url}/grades/stats", headers=invalid_headers)
+    response = requests.get(f"{base_url}/grades/stats", headers=fake_jwt)
     assert response.status_code == 401, f"Expected status code 401, but got {response.status_code}. Response: {response.text}"
+    data = response.json()
+    assert "error" in data, "Response does not contain 'error' key"
+    assert data["error"] == "Not authorized", "Expected 'error' message to be 'Not authorized'"
 
 
 def test_get_grades_stats_forbidden(load_config):
     base_url = load_config["api_url"]
-
     response = requests.get(f"{base_url}/grades/stats")
     assert response.status_code == 403, f"Expected status code 403, but got {response.status_code}. Response: {response.text}"
+    data = response.json()
+    assert "error" in data, "Response does not contain 'error' key"
+    assert data["error"] == "Forbidden", "Expected 'error' message to be 'Forbidden'"
 
 
-def test_get_grades_stats_validation_error(access_token, headers, load_config):  #  тут я не могу вспомнить какой кейс применял,чтобы возращалась 422 от сервера
+def test_get_grades_stats_validation_error(access_token, headers, load_config):
     base_url = load_config["api_url"]
-
-    response = requests.get(f"{base_url}/grades/stats", headers=headers)
+    response = requests.get(f"{base_url}/grades/stats?student_id=not", headers=headers)  #  я передаю неверный тип данных и по идее должно работать
     assert response.status_code == 422, f"Expected status code 422, but got {response.status_code}. Response: {response.text}"
+    data = response.json()
+    assert "error" in data, "Response does not contain 'error' key"
+    assert data["error"] == "Validation Error", "Expected 'error' message to indicate Validation Error"
 
 
-def test_create_grade(setup, access_token, headers, load_config):
+def test_create_grade(setup_class, access_token, headers, load_config):
     base_url = load_config["api_url"]
-
 
     payload_grades = {
-        "teacher_id": 1,
-        "student_id": 1,
-        "grade": 3,
+        "teacher_id": fake.random_int(min=1, max=100),
+        "student_id": fake.random_int(min=1, max=100),
+        "grade": fake.random_int(min=1, max=5)
     }
-
 
     response = requests.post(f"{base_url}/grades/", headers=headers, json=payload_grades)
     assert response.status_code == 201, f"Expected status code 201, but got {response.status_code}. Response: {response.text}"
@@ -52,9 +57,22 @@ def test_get_grades(access_token, headers, load_config):
     assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}. Response: {response.text}"
 
 
-def test_delete_group(setup_delete, access_token, headers, load_config):
+@pytest.fixture(scope="function", autouse=False)
+def setup_delete_group(access_token, headers, load_config):
     base_url = load_config["api_url"]
-    group_id = setup_delete
+    payload_group = {
+        "name": fake.word()
+
+    }
+    response = requests.post(f"{base_url}/groups/", headers=headers, json=payload_group)
+    group_id = response.json().get("id")
+    return group_id
+
+def test_delete_group(setup_delete_group, access_token, headers, load_config):
+    base_url = load_config["api_url"]
+    group_id = setup_delete_group
 
     response = requests.delete(f"{base_url}/groups/{group_id}/", headers=headers)
     assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}. Response: {response.text}"
+    response_check = requests.get(f"{base_url}/groups/{group_id}/", headers=headers)
+    assert response_check.status_code == 404, f"Expected status code 404, but got {response_check.status_code}. Response: {response_check.text}"
